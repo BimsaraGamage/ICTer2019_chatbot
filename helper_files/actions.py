ab09470441db3a7a49ab624204e1ad2ca1b3d80f
bimsaragamage <gamagebimsara@gmail.com>
2019-09-02 00:46:07 +0530

    update actions

--- helper_files/actions.py
+++ helper_files/actions.py
@@ -24,6 +24,7 @@ class ActionWeather(Action):
 		humidity = current['current']['humidity']
 		wind_mph = current['current']['wind_mph']
 
+		# It is currently Partly cloudy in Colombo at the moment. The temperature is 28.0 degrees, the humidity is 84% and the wind speed is 8.1 mph.
 		response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
 						
 		dispatcher.utter_message(response)
